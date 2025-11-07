97e3d6e3759d1d260bd7b4f20614e82ab218290a
Gon <gonzalolucchi2@gmail.com>
2025-11-07 17:42:45 -0300

    scaffold User

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+
+  #:full_name ::= Nombre completo de un usuario
+  validates :full_name, :presence => true, :format => { :with => /\A[a-zA-Z]+\z/,
+      :message => "Only letters allowed" }
+
+  #:email ::= Correo electrónico de un usuario
+  validates :email, :presence => true, :uniqueness => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+      :message => "Not an email" }
+
+  #:password_digest ::= Contraseña hasheada
+  validates :password, :presence => true
+end
