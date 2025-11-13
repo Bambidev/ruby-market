03e304d5d52587bea6c4fb6087609fe3f50b1c56
Gon <gonzalolucchi2@gmail.com>
2025-11-13 16:34:58 -0300

    Validadores y Relaciones para Genre

--- ruby-on-records/app/models/genre.rb
+++ ruby-on-records/app/models/genre.rb
@@ -1,2 +1,11 @@
 class Genre < ApplicationRecord
+  # === Relaciones === #
+
+  # Un Género puede pertenecer a varios Discos
+  has_and_belongs_to_many :disks
+
+  # === Validadores === #
+
+  # :name ::= nombre del género musical
+  validates :name, presence: true
 end
